ffd8ac83e32667103a3dccfa001a133f5062b101
Esteban de la Maza <edelamaza@me.com>
2020-09-12 16:40:43 -0500

    added struct

--- main.cpp
+++ main.cpp
@@ -5,20 +5,48 @@
 
 using namespace std;
 
-void ReplaceMonths (vector<string> &vect) {
+struct Register
+{
+    int date;
+    string reg;
 
+    Register(int date,string reg) {
+        this->date = date;
+        this->reg = reg;
+    }
+};
+
+Register make_Register(int x, string y) {
+    Register newReg = {x, y};
+    return newReg;
 }
 
-void sort(vector<string> &vect) {
-
+void sort(vector<string> &vect)
+{
 }
 
-int main() {
+string convertMonth(string month) { 
+	if (month == "Jun") { return "6"; }
+	else if (month == "Jul") { return "7"; }
+	else if (month == "Aug") { return "8"; }
+	else if (month == "Sep") { return "9"; }
+	else if (month == "Oct") { return "10"; }
+}
+
+int formattedLine(string singleLine) {
+    return stoi(convertMonth(singleLine.substr(0, 2)) + singleLine.substr(4, 5) + singleLine.substr(7, 8) + singleLine.substr(10, 11) + singleLine.substr(13, 14));
+}
+
+
+
+int main()
+{
     ifstream file("bitacora.txt", ios::binary);
-    vector<string> contents;
+    vector<Register> contents;
     string singleLine;
     while (getline(file, singleLine))
-        contents.push_back(singleLine);
+    {
+        contents.push_back(make_Register(formattedLine(singleLine), singleLine.substr(16)));    }
 
     cout << contents.size() << " lines in memory";
 
@@ -27,13 +55,15 @@ int main() {
     //ReplaceMonths(contents);
     //sort(contents);
 
-
+            
+    /*        
     ofstream sorted("sorted.txt", ios::binary);
-    for (vector<string>::iterator it = contents.begin(); it !=contents.end(); it++){
-        sorted << *it << endl;
-    }
+        for (vector<string>::iterator it = contents.begin(); it != contents.end(); it++)
+        {
+            sorted << *it << endl;
+        }
 
-    sorted.close();
-    
-    return 0;
+        sorted.close();
+*/
+        return 0;
 }
